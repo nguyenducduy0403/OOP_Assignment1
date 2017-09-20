ac2ac26659008ae07e432c46c08e9bc5b728a0c0
nguyenducduy0403 <32127543+nguyenducduy0403@users.noreply.github.com>
2017-09-20 16:51:11 +0700

    Create Character.cs

---
+++ Source/Assets/Scripts/Classes/Character/Character.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Class quản lý nhân vật nói chung
+//Một nhân vật có thể là Playable Character hoặc Non-playable character
+public class Character : MonoBehaviour {
+    //Mô tả đặc tính của nhân vật
+    private string name;
+    private float HP;
+    private float MP;
+    private bool isDead = false;
+
+    //Getter - Setter
+
+}
